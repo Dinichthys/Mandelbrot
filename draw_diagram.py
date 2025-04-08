67efaf068baa2a4dba7374d47499f4c431f26022
dinichthys <den2005425@gmail.com>
2025-04-08 12:57:09 +0300

    README Update

--- draw_diagram.py
+++ draw_diagram.py
@@ -14,10 +14,10 @@ delta = [0, 0, 0, 0]
 frequency = 2000000000
 avg = [0, 0, 0, 0]
 
-file_names = ['data/plot_Naive_clang++_O2.png',
-              'data/plot_Array_clang++_O2.png',
-              'data/plot_256_clang++_O3.png',
-              'data/plot_256_full_pipeline_clang++_O2.png']
+file_names = ['data/plot_256_full_pipeline_G++_O2.png',
+              'data/plot_256_full_pipeline_G++_O3.png',
+              'data/plot_256_full_pipeline_clang++_O2.png',
+              'data/plot_256_full_pipeline_clang++_O3.png']
 graph_names = ['Наивная реализация',
                'Реализация на массивах',
                'Реализация с интринсиками размера 256 бит',
@@ -30,127 +30,81 @@ plot_stable_names = ['Naive_stable.png',
 
 fig_size = 10,6
 
-version_num = 0
-with open('Output_1.txt', 'r') as f:
-   data = [float(line.strip()) for line in f]
-
-x = np.empty(1000, dtype=np.int16)
-
-for i in range (1, 1001):
-    x [i-1] = i
-
-plt.figure(figsize=(fig_size))
-plt.plot (x[:500], data[250:750])
-plt.title("Наивная версия в компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
-plt.ylabel('Номер итерации')
-plt.xlabel('Задержка (такты)')
-plt.savefig(plot_stable_names [version_num], dpi = 300)
-plt.clf ()
-
-version_num = 1
-with open('Output_2.txt', 'r') as f:
-   data = [float(line.strip()) for line in f]
-
-x = np.empty(1000, dtype=np.int16)
-
-for i in range (1, 1001):
-    x [i-1] = i
-
-plt.figure(figsize=(fig_size))
-plt.plot (x[:500], data[250:750])
-plt.title("Реализация на массивах в компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
-plt.ylabel('Номер итерации')
-plt.xlabel('Задержка (такты)')
-plt.savefig(plot_stable_names [version_num], dpi = 300)
-plt.clf ()
-
-version_num = 2
-with open('Output_3.txt', 'r') as f:
-   data = [float(line.strip()) for line in f]
-
-x = np.empty(1000, dtype=np.int16)
-
-for i in range (1, 1001):
-    x [i-1] = i
-
-plt.figure(figsize=(fig_size))
-plt.plot (x[:500], data[250:750])
-plt.title("Реализация с интринсиками размера 256\n в компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
-plt.ylabel('Номер итерации')
-plt.xlabel('Задержка (такты)')
-plt.savefig(plot_stable_names [version_num], dpi = 300)
-plt.clf ()
-
-version_num = 3
-with open('Output_4.txt', 'r') as f:
-   data = [float(line.strip()) for line in f]
-
-x = np.empty(1000, dtype=np.int16)
-
-for i in range (1, 1001):
-    x [i-1] = i
-
-plt.figure(figsize=(fig_size))
-plt.plot (x[:500], data[300:800])
-plt.title("Реализация с интринсиками размера 256 бит с полным конвейером\nв компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
-plt.ylabel('Номер итерации')
-plt.xlabel('Задержка (такты)')
-plt.savefig(plot_stable_names [version_num], dpi = 300)
-plt.clf ()
-
-# with open('Output_1_bolt.txt', 'r') as f:
-#    data = [float(line.strip()) for line in f]
-#
 # version_num = 0
-# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black')# range= (3.955*10**8, 3.975*10**8))
-# plt.grid(axis='y', linestyle='--')
-# plt.title("Наивная версия в компиляторе G++ с флагом -O2 с LLVM bolt")
-# plt.xlabel('Задержка (такты)')
-# plt.ylabel('Количество повторений')
-# plt.savefig(file_names [version_num], dpi = 300)
-# plt.clf ()
-#
-# for i in range (250, 751):
-#     avg[version_num] += data [i]
-# avg[version_num] /= 500
-#
-# for i in range (250, 751):
-#     delta [version_num] += (avg [version_num] - data [i])**2
-# delta [version_num] /= 500
-# delta [version_num] = m.sqrt (delta [version_num])
-#
-# print ('Величина равна ', avg [version_num], ' а погрешность ',version_num,' версии равна ', int (round_significant (delta [version_num])))
-#
-# with open('Output_2_bolt.txt', 'r') as f:
+# with open('Output_1.txt', 'r') as f:
 #    data = [float(line.strip()) for line in f]
+#
+# x = np.empty(1000, dtype=np.int16)
+#
+# for i in range (1, 1001):
+#     x [i-1] = i
+#
+# plt.figure(figsize=(fig_size))
+# plt.plot (x[:500], data[250:750])
+# plt.title("Наивная версия в компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
+# plt.ylabel('Номер итерации')
+# plt.xlabel('Задержка (такты)')
+# plt.savefig(plot_stable_names [version_num], dpi = 300)
+# plt.clf ()
 #
 # version_num = 1
-# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black')# range= (1.450*10**8, 1.475*10**8))
-# plt.grid(axis='y', linestyle='--')
-# plt.title("Реализация на массивах в компиляторе G++ с флагом -O2 с LLVM bolt")
-# plt.xlabel('Задержка (такты)')
-# plt.ylabel('Количество повторений')
-# plt.savefig(file_names [version_num], dpi = 300)
-# plt.clf ()
-#
-# for i in range (250, 751):
-#     avg[version_num] += data [i]
-# avg[version_num] /= 500
-#
-# for i in range (250, 751):
-#     delta [version_num] += (avg [version_num] - data [i])**2
-# delta [version_num] /= 500
-# delta [version_num] = m.sqrt (delta [version_num])
-#
-# print ('Величина равна ', avg [version_num], ' а погрешность ',version_num,' версии равна ', int (round_significant (delta [version_num])))
-
-# with open('Output_3_clang_2.txt', 'r') as f:
+# with open('Output_2.txt', 'r') as f:
 #    data = [float(line.strip()) for line in f]
+#
+# x = np.empty(1000, dtype=np.int16)
+#
+# for i in range (1, 1001):
+#     x [i-1] = i
+#
+# plt.figure(figsize=(fig_size))
+# plt.plot (x[:500], data[250:750])
+# plt.title("Реализация на массивах в компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
+# plt.ylabel('Номер итерации')
+# plt.xlabel('Задержка (такты)')
+# plt.savefig(plot_stable_names [version_num], dpi = 300)
+# plt.clf ()
 #
 # version_num = 2
-# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black',range= (7.120*10**7, 7.150*10**7))
+# with open('Output_3.txt', 'r') as f:
+#    data = [float(line.strip()) for line in f]
+#
+# x = np.empty(1000, dtype=np.int16)
+#
+# for i in range (1, 1001):
+#     x [i-1] = i
+#
+# plt.figure(figsize=(fig_size))
+# plt.plot (x[:500], data[250:750])
+# plt.title("Реализация с интринсиками размера 256\n в компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
+# plt.ylabel('Номер итерации')
+# plt.xlabel('Задержка (такты)')
+# plt.savefig(plot_stable_names [version_num], dpi = 300)
+# plt.clf ()
+
+# version_num = 3
+# with open('Output_4.txt', 'r') as f:
+#    data = [float(float(line.strip())/1.5) for line in f]
+#
+# x = np.empty(1000, dtype=np.int16)
+#
+# for i in range (1, 1001):
+#     x [i-1] = i
+#
+# plt.figure(figsize=(fig_size))
+# plt.plot (x[:500], data[300:800])
+# plt.title("Реализация с интринсиками размера 256 бит с полным конвейером\nв компиляторе G++ с флагом -O2\n Задержка в тактах в зависимости от номера итерации")
+# plt.ylabel('Номер итерации')
+# plt.xlabel('Задержка (такты)')
+# plt.savefig(plot_stable_names [version_num], dpi = 300)
+# plt.clf ()
+
+# with open('Output_4.txt', 'r') as f:
+#    data = [float(float(line.strip())/1.5) for line in f]
+#
+# version_num = 0
+# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black', range= (2.70*10**7, 2.74*10**7))
 # plt.grid(axis='y', linestyle='--')
-# plt.title("Реализация с интринсиками размера 256\n в компиляторе clang++ с флагом -O3")
+# plt.title("Реализация с интринсиками размера 256 бит с полным конвейером\nв компиляторе G++ с флагом -O2")
 # plt.xlabel('Задержка (такты)')
 # plt.ylabel('Количество повторений')
 # plt.savefig(file_names [version_num], dpi = 300)
@@ -166,12 +120,35 @@ plt.clf ()
 # delta [version_num] = m.sqrt (delta [version_num])
 #
 # print ('Величина равна ', avg [version_num], ' а погрешность ',version_num,' версии равна ', int (round_significant (delta [version_num])))
-
-# with open('Output_4_clang_O3.txt', 'r') as f:
-#    data = [float(line.strip()) for line in f]
-
-# version_num = 3
-# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black', range= (4.45*10**7, 4.55*10**7))
+#
+# with open('Output_4_2.txt', 'r') as f:
+#    data = [float(float(line.strip())/1.5) for line in f]
+#
+# version_num = 1
+# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black', range= (2.70*10**7, 2.74*10**7))
+# plt.grid(axis='y', linestyle='--')
+# plt.title("Реализация с интринсиками размера 256 бит с полным конвейером\nв компиляторе G++ с флагом -O3")
+# plt.xlabel('Задержка (такты)')
+# plt.ylabel('Количество повторений')
+# plt.savefig(file_names [version_num], dpi = 300)
+# plt.clf ()
+#
+# for i in range (250, 751):
+#     avg[version_num] += data [i]
+# avg[version_num] /= 500
+#
+# for i in range (250, 751):
+#     delta [version_num] += (avg [version_num] - data [i])**2
+# delta [version_num] /= 500
+# delta [version_num] = m.sqrt (delta [version_num])
+#
+# print ('Величина равна ', avg [version_num], ' а погрешность ',version_num,' версии равна ', int (round_significant (delta [version_num])))
+#
+# with open('Output_4_clang.txt', 'r') as f:
+#    data = [float(float(line.strip())/1.5) for line in f]
+#
+# version_num = 2
+# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black', range= (2.96*10**7, 3.04*10**7))
 # plt.grid(axis='y', linestyle='--')
 # plt.title("Реализация с интринсиками размера 256 бит с полным конвейером\nв компиляторе clang++ с флагом -O2")
 # plt.xlabel('Задержка (такты)')
@@ -189,41 +166,64 @@ plt.clf ()
 # delta [version_num] = m.sqrt (delta [version_num])
 #
 # print ('Величина равна ', avg [version_num], ' а погрешность ',version_num,' версии равна ', int (round_significant (delta [version_num])))
+#
+# with open('Output_4_clang_2.txt', 'r') as f:
+#    data = [float(float(line.strip())/1.5) for line in f]
+#
+# version_num = 3
+# plt.hist(data [250 : 751], bins = 40, color = 'blue', edgecolor = 'black', range= (2.98*10**7, 3.01*10**7))
+# plt.grid(axis='y', linestyle='--')
+# plt.title("Реализация с интринсиками размера 256 бит с полным конвейером\nв компиляторе clang++ с флагом -O3")
+# plt.xlabel('Задержка (такты)')
+# plt.ylabel('Количество повторений')
+# plt.savefig(file_names [version_num], dpi = 300)
+# plt.clf ()
+#
+# for i in range (250, 751):
+#     avg[version_num] += data [i]
+# avg[version_num] /= 500
+#
+# for i in range (250, 751):
+#     delta [version_num] += (avg [version_num] - data [i])**2
+# delta [version_num] /= 500
+# delta [version_num] = m.sqrt (delta [version_num])
+#
+# print ('Величина равна ', avg [version_num], ' а погрешность ',version_num,' версии равна ', int (round_significant (delta [version_num])))
 
-# avg = [1, 3.84, 6.08, 9.71]
-#
-# plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
-# plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
-# plt.title("Компилятор G++ с флагом -O2")
-#
-# plt.savefig("data/g++_O3.png", dpi = 300)
-# plt.clf()
-#
-# avg = [1, 2.71, 6.08, 9.7]
-#
-# plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
-# plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
-# plt.title("Компилятор G++ с флагом -O3")
-#
-# plt.savefig("data/g++_O3.png", dpi = 300)
-# plt.clf()
-#
-# avg = [1, 3.30, 5.46, 8.6]
-#
-# plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
-# plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
-# plt.title("Компилятор clang++ с флагом -O2")
-#
-# plt.savefig("data/clang++_O2.png", dpi = 300)
-# plt.clf()
-#
-# avg = [1, 3.30, 5.45, 8.69]
-#
-# plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
-# plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
-# plt.title("Компилятор clang++ с флагом -O3")
-#
-# plt.savefig("data/clang++_O3.png", dpi = 300)
+avg = [1, 3.84, 6.08, 14.58]
+
+plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
+plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
+plt.title("Компилятор G++ с флагом -O2")
+
+plt.savefig("data/g++_O2.png", dpi = 300)
+plt.clf()
+
+avg = [1, 2.71, 6.08, 14.56]
+
+plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
+plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
+plt.title("Компилятор G++ с флагом -O3")
+
+plt.savefig("data/g++_O3.png", dpi = 300)
+plt.clf()
+
+avg = [1, 3.30, 5.46, 12.9]
+
+plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
+plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
+plt.title("Компилятор clang++ с флагом -O2")
+
+plt.savefig("data/clang++_O2.png", dpi = 300)
+plt.clf()
+
+avg = [1, 3.30, 5.45, 13.0]
+
+plt.bar (['Naive', 'Array', 'SIMD 256', 'SIMD 256\nFull Pipeline'], avg)
+plt.ylabel ('Ускорение программы в зависимости\n от способа оптимизации в разах\n относительно наивной версии')
+plt.title("Компилятор clang++ с флагом -O3")
+
+plt.savefig("data/clang++_O3.png", dpi = 300)
 
 
 #
